1532ec5d2ba69b5dd4ff9f355fe09753c40fb400
Howard Mao <zhehao.mao@gmail.com>
2014-01-14 12:21:04 -0500

    fix off-by-one error in controller

--- rtl/drivers/i2c_controller.v
+++ rtl/drivers/i2c_controller.v
@@ -21,12 +21,12 @@ assign i2c_sclk = (!clock_en) || sclk_divider[6];
 wire midlow = (sclk_divider == 7'h1f);
 
 reg sdat = 1'b1;
-assign i2c_sdat = (sdat) ? 1'bz : 1'b0;
+assign i2c_sdat = sdat;
 
 reg [2:0] acks;
 
 assign ack = (acks == 3'b000);
-assign done = (stage == 5'd30);
+assign done = (stage == 5'd29);
 
 always @(posedge clk) begin
     if (start) begin
@@ -40,15 +40,15 @@ always @(posedge clk) begin
         if (sclk_divider == 7'd127) begin
             sclk_divider <= 7'd0;
 
-            if (stage != 5'd30)
+            if (stage != 5'd29)
                 stage <= stage + 1'b1;
 
             case (stage)
                 5'd0:  clock_en <= 1'b1;
-                5'd9: acks[0] <= i2c_sclk;
+                5'd9:  acks[0] <= i2c_sclk;
                 5'd18: acks[1] <= i2c_sclk;
-                5'd28: acks[2] <= i2c_sclk;
-                5'd29: clock_en <= 1'b0;
+                5'd27: acks[2] <= i2c_sclk;
+                5'd28: clock_en <= 1'b0;
             endcase
         end else
             sclk_divider <= sclk_divider + 1'b1;
@@ -67,7 +67,7 @@ always @(posedge clk) begin
                 5'd7:  sdat <= data[17];
                 5'd8:  sdat <= data[16];
                 // ack 1
-                5'd9:  sdat <= 1'b1;
+                5'd9:  sdat <= 1'bz;
                 // byte 2
                 5'd10: sdat <= data[15];
                 5'd11: sdat <= data[14];
@@ -78,7 +78,7 @@ always @(posedge clk) begin
                 5'd16: sdat <= data[9];
                 5'd17: sdat <= data[8];
                 // ack 2
-                5'd18: sdat <= 1'b1;
+                5'd18: sdat <= 1'bz;
                 // byte 3
                 5'd19: sdat <= data[7];
                 5'd20: sdat <= data[6];
@@ -87,12 +87,12 @@ always @(posedge clk) begin
                 5'd23: sdat <= data[3];
                 5'd24: sdat <= data[2];
                 5'd25: sdat <= data[1];
-                5'd27: sdat <= data[0];
+                5'd26: sdat <= data[0];
                 // ack 3
-                5'd28: sdat <= 1'b1;
+                5'd27: sdat <= 1'bz;
                 // stop
-                5'd29: sdat <= 1'b0;
-                5'd30: sdat <= 1'b1;
+                5'd28: sdat <= 1'b0;
+                5'd29: sdat <= 1'b1;
             endcase
         end
     end
