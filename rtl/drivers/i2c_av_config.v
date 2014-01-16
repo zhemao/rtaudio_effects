b66c5652150b80ce2c38ed80f567573b4fcc9d76
Howard Mao <zhehao.mao@gmail.com>
2014-01-16 17:31:08 -0500

    get rid of MIC boost

--- rtl/drivers/i2c_av_config.v
+++ rtl/drivers/i2c_av_config.v
@@ -35,7 +35,7 @@ always @(*) begin
         4'h2: lut_data <= 16'h0217; // right input
         4'h3: lut_data <= 16'h0479; // left output
         4'h4: lut_data <= 16'h0679; // right output
-        4'h5: lut_data <= 16'h08d5; // analog path
+        4'h5: lut_data <= 16'h08d4; // analog path
         4'h6: lut_data <= 16'h0a04; // digital path
         4'h7: lut_data <= 16'h0e01; // digital IF
         4'h8: lut_data <= 16'h1020; // sampling rate
